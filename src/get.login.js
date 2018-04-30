7f1f1a9ae6753cb517f4ab79b7e90db5bf3a6e67
maxran <max-xuran@hotmail.com>
2018-04-30 12:05:49 +0800

    ques

--- src/get.login.js
+++ src/get.login.js
@@ -1,5 +1,14 @@
 var path = require('path')
 module.exports = (req,res)=>{   
-    res.sendFile( path.resolve('./')+'/public/login.html')
-    
+     if(req.session.username){
+        res.cookie("message",JSON.stringify({
+            status: 'error',
+            link: '/index',
+            linkText: '返回管理首页',
+            msg: '您已经登陆了'
+        }))
+        res.sendFile( path.resolve('./')+'/public/message.html')
+    } else{
+        res.sendFile( path.resolve('./')+'/public/login.html')
+    }
 }
